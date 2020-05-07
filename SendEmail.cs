fb2f33c5b7ebd91ac2c2d2e3da584def4211a198
Oscar Gutierrez C <osquinor@hotmail.com>
2020-05-06 22:32:28 -0600

    Add base code to send request to SendGrid.

--- SendEmail.cs
+++ SendEmail.cs
@@ -9,16 +9,21 @@ using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using System.Net.Http;
 using System.Collections.Generic;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System.Net;
 
 namespace Zetill.Utils
 {
     public class SendEmail
     {
         private readonly HttpClient httpClient;
+        private readonly ILogger<SendEmail> log;
 
-        public SendEmail(HttpClient httpClient)
+        public SendEmail(HttpClient httpClient, ILogger<SendEmail> log)
         {
             this.httpClient = httpClient;
+            this.log = log;
         }
 
         [FunctionName("SendEmail")]
@@ -28,10 +33,10 @@ namespace Zetill.Utils
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync().ConfigureAwait(false);
             var request = JsonConvert.DeserializeObject<SendEmailRequest>(requestBody);
 
-            var hCaptchaSecret = "";
+            var hCaptchaSecret = ""; // TODO: Read from Key Vault.
 
             IEnumerable<KeyValuePair<string, string>> hCaptchaParams = new List<KeyValuePair<string, string>>()
             {
@@ -41,13 +46,44 @@ namespace Zetill.Utils
             };
 
             var hCaptchaVerificationContent = new FormUrlEncodedContent(hCaptchaParams);
-            var responseFromHCaptcha = await this.httpClient.PostAsync(@"https://hcaptcha.com/siteverify", hCaptchaVerificationContent);
+            var responseFromHCaptcha = await this.httpClient.PostAsync(@"https://hcaptcha.com/siteverify", hCaptchaVerificationContent).ConfigureAwait(false);
 
-            if(!responseFromHCaptcha.IsSuccessStatusCode){
+            if(!responseFromHCaptcha.IsSuccessStatusCode)
+            {
+                this.log.LogWarning($"Captcha validation was unsuccessfull. Response Status Code was: {responseFromHCaptcha.StatusCode}. Reason: {responseFromHCaptcha.ReasonPhrase}");
                 return new BadRequestResult();
             }
 
-            // TODO: Add logic to format email and call SendGrid.
+            // var sourceDomainName = "Chrysalis-Tech.com"; // TODO: Read from config.
+            var sourceUserName = "Chrysalis Technology"; // TODO: Read from config.
+            var sourceEmail = "info@chrysalis-tech.com"; // TODO: Read from config.
+
+            var targetDomainName = "Retagri.com"; // TODO: Read from config.
+            var targetUserName = "Retagri S.A."; // TODO: Read from config.
+            var targetEmail = "info@retagri.com"; // TODO: Read from config.
+
+
+
+            var apiKey = Environment.GetEnvironmentVariable("NAME_OF_THE_ENVIRONMENT_VARIABLE_FOR_YOUR_SENDGRID_KEY");  // TODO: Read from Key Vault.
+            var sendgridClient = new SendGridClient(apiKey);
+            var from = new EmailAddress(sourceEmail, sourceUserName);
+            
+            var subject = "Nuevo mensaje recibido";
+            var to = new EmailAddress(targetEmail, targetUserName);
+            
+            var plainTextContent = $@"Le comunicamos que ha recibido una nueva petición de contacto por medio de su sitio: {targetDomainName}."
+                                 + $@"Le mensaje fue enviado por: {request.Name} y dice lo siguiente: {request.Message}."
+                                 + $@"Información de contacto: Email: {request.Email} Número de Teléfono: {request.PhoneNumber}.";
+            
+            var htmlContent = ""; // "<strong>and easy to do anywhere, even with C#</strong>"; // TODO: Consider using HTML
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+            var sendgridResponse = await sendgridClient.SendEmailAsync(msg).ConfigureAwait(false);
+
+            if(sendgridResponse.StatusCode != HttpStatusCode.Accepted)
+            {
+                var responseBody = await sendgridResponse.Body.ReadAsStringAsync().ConfigureAwait(false);
+                this.log.LogError($"Unable to send email. Respose from SendGrid was: {responseBody}");
+            }
 
             return new OkObjectResult("Success");
         }
