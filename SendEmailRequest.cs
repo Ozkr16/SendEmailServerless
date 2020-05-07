754c65114eee9d92ee610586e5a61c33002f5821
Oscar Gutierrez C <osquinor@hotmail.com>
2020-05-06 22:31:54 -0600

    Add extra props to the SendEmailRequest

--- SendEmailRequest.cs
+++ SendEmailRequest.cs
@@ -3,5 +3,9 @@ namespace Zetill.Utils
     public class SendEmailRequest
     {
         public string HCaptchaChallengeResponse { get; internal set; }
+        public object Name { get; internal set; }
+        public object Message { get; internal set; }
+        public object Email { get; internal set; }
+        public object PhoneNumber { get; internal set; }
     }
 }
